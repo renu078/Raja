ab055f2a9746a783bf3d24c302cee914a6b2c569
renu078 <rt2436488@gmail.com>
2024-12-19 15:41:27 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -2,13 +2,13 @@
 #SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7325943272:AAHE02ptyTnD3KdfD7fYsTq1Ro3aecru2kw")
+BOT_TOKEN=("8050966708:AAGRof2DsChsr2VWdXnqn7sTkrq6H5Wan9U")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@rajaraj_04")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5647653901")
+ADMIN_IDS=("7855020275")
 
 
 
